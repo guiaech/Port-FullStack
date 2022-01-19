9e8e6f2eebcdc510add70384506ffefcd82e638a
Guilherme Andrade e Chaves <83043492+guiaech@users.noreply.github.com>
2022-01-19 13:38:48 -0300

    Update index.js

--- src/Data/index.js
+++ src/Data/index.js
@@ -114,6 +114,12 @@ const DATA = [
     image:'https://ms314006.github.io/static/b7a8f321b0bbc07ca9b9d22a7a505ed5/0e6ff/React.jpg',
     link: 'https://gui-dados.netlify.app/',
     address: 2,
+  },{
+    id: '19',
+    title: 'React | Diary Day Trade',
+    image:'https://ms314006.github.io/static/b7a8f321b0bbc07ca9b9d22a7a505ed5/0e6ff/React.jpg',
+    link: 'https://tradediary.netlify.app/',
+    address: 2,
   }
  
   ];
